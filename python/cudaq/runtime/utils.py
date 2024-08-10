936e38352a78979610270300d0f61478f0c2974f
Sachin Pisal <spisal@nvidia.com>
2024-08-09 20:17:15 -0700

    Refactoring utils.py to remove duplicate code

--- python/cudaq/runtime/utils.py
+++ python/cudaq/runtime/utils.py
@@ -17,58 +17,22 @@ import sys
 from typing import List
 
 
-def __isBroadcast(kernel, *args):
-    # kernel could be a PyKernel or PyKernelDecorator
+def __getArgTypes(kernel):
+    argTypes = None
     if isinstance(kernel, PyKernel):
         argTypes = kernel.mlirArgTypes
-        if not argTypes or not args:
-            return False
-
-        # Quick check, if we have a 2d array anywhere, we know this is a broadcast
-        isDefinitelyBroadcast = any(
-            hasattr(arg, "shape") and len(arg.shape) == 2 for arg in args)
-
-        if isDefinitelyBroadcast:
-            # Error check, did the user pass a single value for any of the other arguments
-            for i, arg in enumerate(args):
-                if isinstance(arg, (int, float, bool, str)):
-                    raise RuntimeError(
-                        f"2D array argument provided for an sample or observe broadcast, but argument {i} ({type(arg)}) must be a list."
-                    )
-
-        firstArg = args[0]
-        firstArgTypeIsStdvec = cc.StdvecType.isinstance(argTypes[0])
-        if isinstance(firstArg, (list, List)) and not firstArgTypeIsStdvec:
-            return True
-
-        if hasattr(firstArg, "shape"):
-            if firstArg.ndim == 1 and not firstArgTypeIsStdvec:
-                return True
-
-            if firstArg.ndim == 2:
-                return True
-
-        return False
-
     elif isinstance(kernel, PyKernelDecorator):
         argTypes = kernel.signature
-        if not argTypes or not args:
-            return False
 
-        # Quick check, if we have a 2d array anywhere, we know this
-        # is a broadcast
-        isDefinitelyBroadcast = any(
-            hasattr(arg, "shape") and len(arg.shape) == 2 for arg in args)
+    return argTypes
 
-        if isDefinitelyBroadcast:
-            # Error check, did the user pass a single value for any of the other arguments
-            for i, arg in enumerate(args):
-                if isinstance(arg, (int, float, bool, str)):
-                    raise RuntimeError(
-                        f"2D array argument provided for an observe broadcast, but argument {i} ({type(arg)}) must be a list."
-                    )
 
-        firstArg = args[0]
+def __isFirstArgTypeStdVec(kernel, argTypes):
+    isFirstArgTypeStdVec = None
+
+    if isinstance(kernel, PyKernel):
+        isFirstArgTypeStdVec = cc.StdvecType.isinstance(argTypes[0])
+    elif isinstance(kernel, PyKernelDecorator):
         firstArgType = next(iter(argTypes))
         checkList = [
             list, np.ndarray, List, List[float], List[complex], List[int],
@@ -77,21 +41,47 @@ def __isBroadcast(kernel, *args):
         ]
         ## [PYTHON_VERSION_FIX]
         if sys.version_info >= (3, 9):
-            checkList.extend(
-                [list[float], list[complex], list[int], list[bool],
-                 'list[float]', 'list[complex]', 'list[int]', 'list[bool]'])
-        firstArgTypeIsStdvec = argTypes[firstArgType] in checkList
-        if isinstance(firstArg, (list, List)) and not firstArgTypeIsStdvec:
+            checkList.extend([
+                list[float], list[complex], list[int], list[bool],
+                'list[float]', 'list[complex]', 'list[int]', 'list[bool]'
+            ])
+        isFirstArgTypeStdVec = argTypes[firstArgType] in checkList
+
+    return isFirstArgTypeStdVec
+
+
+def __isBroadcast(kernel, *args):
+    # kernel could be a PyKernel or PyKernelDecorator
+    argTypes = __getArgTypes(kernel)
+    if not argTypes or not args:
+        return False
+
+    # Quick check, if we have a 2d array anywhere, we know this is a broadcast
+    isDefinitelyBroadcast = any(
+        hasattr(arg, "shape") and len(arg.shape) == 2 for arg in args)
+
+    if isDefinitelyBroadcast:
+        # Error check, did the user pass a single value for any of the other arguments
+        for i, arg in enumerate(args):
+            if isinstance(arg, (int, float, bool, str)):
+                raise RuntimeError(
+                    f"2D array argument provided for an sample or observe broadcast, but argument {i} ({type(arg)}) must be a list."
+                )
+
+    firstArg = args[0]
+    firstArgTypeIsStdvec = __isFirstArgTypeStdVec(kernel, argTypes)
+
+    if isinstance(firstArg, (list, List)) and not firstArgTypeIsStdvec:
+        return True
+
+    if hasattr(firstArg, "shape"):
+        if firstArg.ndim == 1 and not firstArgTypeIsStdvec:
             return True
 
-        if hasattr(firstArg, "shape"):
-            if firstArg.ndim == 1 and not firstArgTypeIsStdvec:
-                return True
+        if firstArg.ndim == 2:
+            return True
 
-            if firstArg.ndim == 2:
-                return True
-
-        return False
+    return False
 
 
 def __createArgumentSet(*args):
