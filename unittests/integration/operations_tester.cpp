e8a133f13cd08456adb27149e640d781596c9d64
A.M. Santana <anthonys@nvidia.com>
2024-09-09 15:46:40 +0000

    fill out unit tests for to matrix overload
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>

--- unittests/integration/operations_tester.cpp
+++ unittests/integration/operations_tester.cpp
@@ -10,22 +10,61 @@
 
 #include "cudaq/expressions.h"
 
+cudaq::complex_matrix zero(int size) {
+  auto mat = cudaq::complex_matrix(size, size);
+  mat.set_zero();
+  return mat;
+}
+
+cudaq::complex_matrix id(int size) {
+  auto mat = cudaq::complex_matrix(size, size);
+  for (int i = 0; i < size; i++)
+    mat(i, i) = 1.0 + 0.0j;
+  return mat;
+}
+
 TEST(ExpressionTester, checkPreBuiltElementaryOps) {
-  auto op_id = cudaq::ElementaryOperator::identity(2);
-  auto op_zero = cudaq::ElementaryOperator::zero(2);
+  // Check 2x2 variations.
+  {
+    auto id_2 = cudaq::ElementaryOperator::identity(2);
+    auto zero_2 = cudaq::ElementaryOperator::zero(2);
 
-  auto op_mat = op_id.to_matrix({},{});
-  auto id_mat = op_zero.to_matrix({},{});
+    auto want_id_2 = id_2.to_matrix({}, {});
+    auto want_zero_2 = zero_2.to_matrix({}, {});
+    auto got_id_2 = id(2);
+    auto got_zero_2 = zero(2);
 
-  op_mat.dump();
-  id_mat.dump();
+    ASSERT_TRUE(want_id_2 == got_id_2);
+    ASSERT_TRUE(want_zero_2 == got_zero_2);
+  }
 
-  // // Trying different sizes and just visually confirming for now.
-  // cudaq::ElementaryOperator::identity(3);
-  // cudaq::ElementaryOperator::zero(3);
+  // Check 3x3 variations.
+  {
+    auto id_3 = cudaq::ElementaryOperator::identity(3);
+    auto zero_3 = cudaq::ElementaryOperator::zero(3);
 
-  // cudaq::ElementaryOperator::identity(4);
-  // cudaq::ElementaryOperator::zero(4);
+    auto want_id_3 = id_3.to_matrix({}, {});
+    auto want_zero_3 = zero_3.to_matrix({}, {});
+    auto got_id_3 = id(3);
+    auto got_zero_3 = zero(3);
+
+    ASSERT_TRUE(want_id_3 == got_id_3);
+    ASSERT_TRUE(want_zero_3 == got_zero_3);
+  }
+
+  // Check 4x4 variations.
+  {
+    auto id_4 = cudaq::ElementaryOperator::identity(4);
+    auto zero_4 = cudaq::ElementaryOperator::zero(4);
+
+    auto want_id_4 = id_4.to_matrix({}, {});
+    auto want_zero_4 = zero_4.to_matrix({}, {});
+    auto got_id_4 = id(4);
+    auto got_zero_4 = zero(4);
+
+    ASSERT_TRUE(want_id_4 == got_id_4);
+    ASSERT_TRUE(want_zero_4 == got_zero_4);
+  }
 }
 
 TEST(ExpressionTester, checkCustomElementaryOps) {
